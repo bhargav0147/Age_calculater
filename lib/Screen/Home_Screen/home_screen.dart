371749d38ddeafec93165b2a854106b0f353f23f
Lenovo <bhargavpatel6832@gmail.com>
2023-02-10 11:43:54 +0530

    New

---
+++ lib/Screen/Home_Screen/home_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Homescreen extends StatefulWidget {
+  const Homescreen({Key? key}) : super(key: key);
+
+  @override
+  State<Homescreen> createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: 100,width: 100,
+            color: Colors.blue,
+
+            
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:app1/Screen/Home_Screen/home_screen.dart';
+import 'package:flutter/material.dart';
+void main()
+{
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Homescreen(),
+    )
+  );
+}
