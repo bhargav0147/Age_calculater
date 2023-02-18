22f214c55cd7f3507b51da937448d104f523a63a
Lenovo <bhargavpatel6832@gmail.com>
2023-02-18 13:16:45 +0530

    New

--- lib/Screen/Home_Screen/home_screen.dart
+++ lib/Screen/Home_Screen/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class Homescreen extends StatefulWidget {
@@ -8,18 +10,44 @@ class Homescreen extends StatefulWidget {
 }
 
 class _HomescreenState extends State<Homescreen> {
+  int a=0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            height: 100,width: 100,
-            color: Colors.blue,
 
-            
-          )
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+        title: Center(
+          child: Text(
+            "Age Calculater",
+            style: TextStyle(
+                letterSpacing: 1,
+                fontSize: 20
+            ),
+          ),
+        ),
+        leading: Center(
+          child: Icon(Icons.menu_open),
+        ),
+        actions: [
+          Icon(Icons.account_circle)
         ],
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+            Container(
+              height: 400,width: 400,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10)
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
